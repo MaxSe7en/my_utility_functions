15a496e762e38e62e75dfc954a5846819d0789a4
Maxwell <nanayawfixing@gmail.com>
2023-06-29 10:24:40 +0000

    first commit

---
+++ isempty.js
@@ -0,0 +1,24 @@
+/**
+ * Checks if a value is empty.
+ * @param value - The value to check.
+ * @returns A boolean indicating whether the value is empty.
+ */
+function isEmpty(value) {
+  if (value === null || value === undefined) {
+    return true;
+  }
+
+  if (typeof value === 'string' && value.trim() === '') {
+    return true;
+  }
+
+  if (Array.isArray(value) && value.length === 0) {
+    return true;
+  }
+
+  if (typeof value === 'object' && Object.keys(value).length === 0) {
+    return true;
+  }
+
+  return false;
+}
