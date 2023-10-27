4da80afe6c04fa90231d859b303eedd687f8762c
Maxwell <nanayawfixing@gmail.com>
2023-10-27 10:10:45 +0000

    good

--- divisor.js
+++ divisor.js
@@ -139,4 +139,4 @@ function persistence(num) {
 console.log(persistence(999))
 console.log(persistence(39))
 console.log(persistence(25))
-// console.log(persistence(999))
+console.log(persistence(999))
